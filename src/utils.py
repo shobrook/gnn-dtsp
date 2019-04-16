aa02e69dfe50317a04dab1c84891946dee621ab7
pjdohm <49376289+pjdohm@users.noreply.github.com>
2019-04-15 21:01:56 -0500

    Add files via upload

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import networkx as nx
 import random as rand
 from tsp_solver.greedy import solve_tsp
 
-def create_random_graph(node_range=(5, 9), prob=0.25, weight_range=(0, 10)):
+def create_random_graph(node_range=(5, 9), prob=0.25, weight_range=(1, 10)):
     n_nodes = rand.randint(*node_range)
 
     G = nx.complete_graph(n_nodes)
@@ -33,3 +33,85 @@ def create_dataset(n_examples=20000):
 
         inputs.append(input_graph)
         targets.append(target_graph)
+
+
+
+def to_one_hot(indices, max_value, axis=-1):
+  one_hot = np.eye(max_value)[indices]
+  if axis not in (-1, one_hot.ndim):
+    one_hot = np.moveaxis(one_hot, -1, axis)
+  return one_hot
+
+
+def graph_to_input_target(graph):
+  """Returns 2 graphs with input and target feature vectors for training.
+
+  Args:
+    graph: An `nx.Graph` instance.
+
+  Returns:
+    The input `nx.Graph` instance.
+    The target `nx.Graph` instance.
+
+  Raises:
+    ValueError: unknown node type
+  """
+
+  def create_feature(attr, fields):
+    return np.hstack([np.array(attr[field], dtype=float) for field in fields])
+
+  #input_node_fields = ("solution",)
+  input_edge_fields = ("weight",)
+  #target_node_fields = ("solution",)
+  target_edge_fields = ("solution",)
+
+  input_graph = graph.copy()
+  target_graph = graph.copy()
+
+  solution_length = 0
+  for node_index in graph.nodes():
+    input_graph.add_node(node_index)
+    target_graph.add_node(node_index)
+
+  for receiver, sender, features in graph.edges(data=True):
+    input_graph.add_edge(
+        sender, receiver, features=create_feature(features, input_edge_fields))
+    target_edge = to_one_hot(
+        create_feature(features, target_edge_fields).astype(int), 2)[0]
+    target_graph.add_edge(sender, receiver, features=target_edge)
+    solution_length += features["weight"] * features["solution"]
+
+  input_graph.graph["features"] = np.array([0.0])
+  target_graph.graph["features"] = np.array([solution_length], dtype=float)
+
+  return input_graph, target_graph
+
+def generate_networkx_graphs(num_graphs, node_range=(5, 9), prob=0.25, weight_range=(1, 10)):
+  """Generate graphs for training.
+
+  Args:
+    num_graphs: number of graphs to generate
+    num_range: a 2-tuple with the [lower, upper) number of nodes per
+      graph
+    prob: the probability of removing an edge between any two nodes
+    weight_range: a 2-tuple with the [lower, upper) weight to randomly assign
+        to (non-removed) edges
+
+  Returns:
+    input_graphs: The list of input graphs.
+    target_graphs: The list of output graphs.
+    graphs: The list of generated graphs.
+  """
+
+  input_graphs = []
+  target_graphs = []
+  graphs = []
+
+  for i in range(num_graphs):
+    graph = create_random_graph(node_range, prob, weight_range)
+    graph = get_tsp_solution(graph)
+    input_graph, target_graph = graph_to_input_target(graph)
+    input_graphs.append(input_graph)
+    target_graphs.append(target_graph)
+    graphs.append(graph)
+  return input_graphs, target_graphs, graphs
